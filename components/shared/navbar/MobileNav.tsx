29fa1beb4413ccb4fffcd4d82d88e75dd9259929
Tye Stanley <tyestanley090@gmail.com>
2023-10-07 17:43:59 -0700

    feat: added mobile nav content

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,13 +1,46 @@
-import React from "react";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import { Sheet, SheetContent, SheetClose, SheetTrigger } from "@/components/ui/sheet";
 import { SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const NavContent = () => {
-  return "";
+  const pathname = usePathname();
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map(item => {
+        const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+
+        return (
+          <SheetClose
+            asChild
+            key={item.route}
+          >
+            <Link
+              href={item.route}
+              className={`${
+                isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>{item.label}</p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 };
 
 const MobileNav = () => {
