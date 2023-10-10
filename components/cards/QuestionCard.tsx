9bea40a841d6d79b828fd87ec3721ccee81aa58f
Tye Stanley <tyestanley090@gmail.com>
2023-10-09 23:45:34 -0700

    feat: used the getTimestamp function in the question card component

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import RenderTag from "../shared/RenderTag";
 import Metric from "../shared/Metric";
+import { getTimestamp } from "@/lib/utils";
 
 interface QuestionProps {
   _id: string;
@@ -26,7 +27,9 @@ const QuestionCard = ({ _id, title, tags, author, upvotes, views, answers, creat
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
-          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">{String(createdAt)}</span>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimestamp(createdAt)}
+          </span>
 
           <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">{title}</h3>
@@ -51,7 +54,7 @@ const QuestionCard = ({ _id, title, tags, author, upvotes, views, answers, creat
           imgUrl="/assets/icons/avatar.svg"
           alt="user"
           value={author.name}
-          title=" - asked 1 hour ago"
+          title={` - asked ${getTimestamp(createdAt)}`}
           href={`/profile/${author._id}`}
           isAuthor
           textStyles="body-medium text-dark400_light700"
