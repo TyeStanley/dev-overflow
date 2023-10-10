38e754d9c14ee653fd7a0869cdef94cce5fd4ae1
Tye Stanley <tyestanley090@gmail.com>
2023-10-09 23:48:55 -0700

    feat: created format number function

--- lib/utils.ts
+++ lib/utils.ts
@@ -40,3 +40,15 @@ export const getTimestamp = (createdAt: Date): string => {
     return `${years} ${years === 1 ? "year" : "years"} ago`;
   }
 };
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (num >= 1000000) {
+    const formattedNum = (num / 1000000).toFixed(1);
+    return `${formattedNum}M`;
+  } else if (num >= 1000) {
+    const formattedNum = (num / 1000).toFixed(1);
+    return `${formattedNum}K`;
+  } else {
+    return num.toString();
+  }
+};
