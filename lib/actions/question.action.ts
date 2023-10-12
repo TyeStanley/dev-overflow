3e82f536396b7ff9723961fbeb750017651f0a5d
Tye Stanley <tyestanley090@gmail.com>
2023-10-12 14:29:05 -0700

    feat: added code to create a new question in the server action called createQuestion

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,9 +1,39 @@
 "use server";
 
+import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
 import { connectToDatabase } from "../mongoose";
 
 export async function createQuestion(params: any) {
   try {
     connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    // Create an interaction record for the user's ask_question action
+
+    // Increment author's reputation by +5 for creating a question
   } catch (error) {}
 }
