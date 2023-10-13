ce2ef825ad493a5ab3688a84264417bf75502871
Tye Stanley <tyestanley090@gmail.com>
2023-10-13 10:50:55 -0700

    fix: deployment issues are fixed

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -62,7 +62,7 @@ export async function deleteUser(params: DeleteUserParams) {
     if (!user) throw new Error("User not found");
 
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
