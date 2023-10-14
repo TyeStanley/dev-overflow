26b862f04d16771569f26bca88fb6d9659ba93af
Tye Stanley <tyestanley090@gmail.com>
2023-10-13 22:21:09 -0700

    feat: created getAllUsers server action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,10 +1,10 @@
 "use server";
 
 import User from "@/database/user.model";
-import { connectToDatabase } from "../mongoose";
-import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
-import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+import { connectToDatabase, connectToDatabase } from "../mongoose";
+import { CreateUserParams, DeleteUserParams, GetAllUsersParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
 export async function getUserById(params: any) {
   try {
@@ -72,6 +72,21 @@ export async function deleteUser(params: DeleteUserParams) {
     const deletedUser = await User.findByIdAndDelete(user._id);
 
     return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({}).sort({ createdAt: -1 });
+
+    return { users };
   } catch (error) {
     console.log(error);
     throw error;
