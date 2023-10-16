ba9c3f72724e2690eea94e6772a71aecfa1d7bfa
Tye Stanley <tyestanley090@gmail.com>
2023-10-16 16:53:57 -0700

    feat: created ParseHTML component and installed Prismjs and htmlParser

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,4 +1,5 @@
 import Metric from "@/components/shared/Metric";
+import ParseHTML from "@/components/shared/ParseHTML";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 import Image from "next/image";
@@ -56,6 +57,8 @@ const Page = async ({ params, searchParams }) => {
           textStyles="small-medium text-dark400_light800"
         />
       </div>
+
+      <ParseHTML data={result.content} />
     </>
   );
 };
