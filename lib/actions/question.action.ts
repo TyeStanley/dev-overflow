2f71d7e1cf28462a27e91d61f41ed7a71ad62b56
Tye Stanley <tyestanley090@gmail.com>
2023-10-17 09:54:02 -0700

    feat: added downvoteQuestion server action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -105,3 +105,35 @@ export async function upvoteQuestion(params: QuestionVoteParams) {
     throw error;
   }
 }
+
+export async function downvoteQuestion(params: QuestionVoteParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId, userId, hasupVoted, hasdownVoted, path } = params;
+
+    let updateQuery = {};
+
+    if (hasdownVoted) {
+      updateQuery = { $pull: { downvotes: userId } };
+    } else if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+        $push: { downvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { downvotes: userId } };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, { new: true });
+
+    if (!question) throw new Error("Question not found");
+
+    // TODO: Increment author's reputation by +10 for upvoting a question
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
