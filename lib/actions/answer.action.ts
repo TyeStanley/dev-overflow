7a8b987da182d506065aedf28a1ef37ca7737ba8
Tye Stanley <tyestanley090@gmail.com>
2023-10-18 09:21:48 -0700

    feat: created deleteAnswer server action

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import Answer from "@/database/answer.model";
-import { connectToDatabase } from "../mongoose";
-import { AnswerVoteParams, CreateAnswerParams, GetAnswersParams } from "./shared.types";
 import Question from "@/database/question.model";
+import Interaction from "@/database/interaction.model";
+import { connectToDatabase } from "../mongoose";
+import { AnswerVoteParams, CreateAnswerParams, DeleteAnswerParams, GetAnswersParams } from "./shared.types";
 import { revalidatePath } from "next/cache";
 
 export async function createAnswer(params: CreateAnswerParams) {
@@ -104,6 +105,27 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
     if (!answer) throw new Error("Answer not found");
 
     // TODO: Increment author's reputation by +10 for upvoting a answer
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteAnswer(params: DeleteAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { answerId, path } = params;
+
+    const answer = await Answer.findById(answerId);
+
+    if (!answer) throw new Error("Answer not found");
+
+    await answer.deleteOne({ _id: answerId });
+    await Question.updateMany({ _id: answer.question }, { $pull: { answers: answerId } });
+    await Interaction.deleteMany({ answer: answerId });
 
     revalidatePath(path);
   } catch (error) {
