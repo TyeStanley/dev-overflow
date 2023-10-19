b520b62ef79caad45225d2ac003d70b46b31ac64
Tye Stanley <tyestanley090@gmail.com>
2023-10-19 00:08:59 -0700

    feat: created getHotQuestions server action and implemented getting the top questions

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,14 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import RenderTag from "./RenderTag";
-
-const hotQuestions = [
-  { _id: "1", title: "How do I use express as a custom server in NextJS" },
-  { _id: "2", title: "Cascading Deletes in SQLAlchemy?" },
-  { _id: "3", title: "How to Perfectly Center a Div with Tailwind CSS?" },
-  { _id: "4", title: "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?" },
-  { _id: "5", title: "Redux Toolkit Not Updating State as Expected" },
-];
+import { getHotQuestions } from "@/lib/actions/question.action";
 
 const popularTags = [
   { _id: "1", name: "javascript", totalQuestions: 5 },
@@ -18,7 +11,8 @@ const popularTags = [
   { _id: "5", name: "redux", totalQuestions: 10 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -27,7 +21,7 @@ const RightSidebar = () => {
           {hotQuestions.map(question => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">{question.title}</p>
