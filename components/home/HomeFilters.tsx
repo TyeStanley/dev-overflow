4ce6a5e7ad59a7a4731ead9711bc13ce18a5d77c
Tye Stanley <tyestanley090@gmail.com>
2023-10-19 11:20:41 -0700

    feat: added functionality to the filters on home page to show on url

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -2,9 +2,37 @@
 
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "newest";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
 
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
@@ -15,6 +43,7 @@ const HomeFilters = () => {
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active === item.value ? "bg-primary-100 text-primary-500" : "bg-light-800 text-light-500"
           }`}
+          onClickCapture={() => handleTypeClick(item.value)}
         >
           {item.name}
         </Button>
