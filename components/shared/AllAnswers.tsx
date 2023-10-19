2623623e0a7b1192ab15f4416d39ceeac8910509
Tye Stanley <tyestanley090@gmail.com>
2023-10-19 12:46:17 -0700

    feat: added sorting to answers

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -17,7 +17,11 @@ interface Props {
 }
 
 const AllAnswers = async ({ questionId, userId, totalAnswers, page, filter }: Props) => {
-  const result = await getAnswers({ questionId });
+  const result = await getAnswers({
+    questionId,
+    page: page ? +page : 1,
+    sortBy: filter,
+  });
 
   return (
     <div className="mt-11">
