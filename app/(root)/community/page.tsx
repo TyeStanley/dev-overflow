bd5eaf9a97968c3f5ba8d4d1361d1525ff855ec2
Tye Stanley <tyestanley090@gmail.com>
2023-10-19 01:27:36 -0700

    feat: added the ability to search for users on the community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -3,10 +3,14 @@ import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { UserFilters } from "@/constants/filters";
 import { getAllUsers } from "@/lib/actions/user.action";
+import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 
-const Page = async () => {
-  const result = await getAllUsers({});
+const Page = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getAllUsers({
+    searchQuery: searchParams.q,
+  });
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
