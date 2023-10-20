6d2fc8fe360440fd12a9f35b06a07419e35091e8
Tye Stanley <tyestanley090@gmail.com>
2023-10-19 18:22:39 -0700

    feat: updated renderLink to generate dynamic path links

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -6,16 +6,13 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import GlobalFilters from "./GlobalFilters";
+import { globalSearch } from "@/lib/actions/general.action";
 
 const GlobalResult = () => {
   const searchParams = useSearchParams();
 
   const [isLoading, setIsLoading] = useState(false);
-  const [result, setResult] = useState([
-    { type: "question", id: 1, title: "next.js question" },
-    { type: "tag", id: 1, title: "Nextjs" },
-    { type: "user", id: 1, title: "jsm" },
-  ]);
+  const [result, setResult] = useState([]);
 
   const global = searchParams.get("global");
   const type = searchParams.get("type");
@@ -26,17 +23,32 @@ const GlobalResult = () => {
       setIsLoading(true);
 
       try {
-        //
+        const res = await globalSearch({ query: global, type });
+
+        setResult(JSON.parse(res));
       } catch (error) {
         console.log(error);
       } finally {
         setIsLoading(false);
       }
     };
+
+    if (global) fetchResult();
   }, [global, type]);
 
   const renderLink = (type: string, id: string) => {
-    return "/";
+    switch (type) {
+      case "question":
+        return `/question/${id}`;
+      case "answer":
+        return `/question/${id}`;
+      case "user":
+        return `/profile/${id}`;
+      case "tag":
+        return `/tags/${id}`;
+      default:
+        return "/";
+    }
   };
 
   return (
@@ -58,7 +70,7 @@ const GlobalResult = () => {
             {result.length > 0 ? (
               result.map((item: any, index: number) => (
                 <Link
-                  href={renderLink("type", "id")}
+                  href={renderLink(item.type, item.id)}
                   key={item.type + item.id + index}
                   className="flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 hover:dark:bg-dark-500/50"
                 >
